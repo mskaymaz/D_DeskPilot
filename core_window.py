0c565d134d6939c47420a795c271498671aefff3
MSK <mustafasalihkaymaz@gmail.com>
2026-07-02 11:14:12 +0300

    Extract window runtime behavior

--- core_window.py
+++ core_window.py
@@ -31,13 +31,14 @@ from window_mouse_mixin import WindowMouseMixin
 from window_lifecycle_mixin import WindowLifecycleMixin
 from window_settings_mixin import WindowSettingsMixin
 from window_init_mixin import WindowInitMixin
+from window_runtime_mixin import WindowRuntimeMixin
 from serbest_duzen import SerbestDuzenKarishimi
 
 def move_window_safely(window, settings):
     """DeskPilot.py tarafından kullanılan güvenli taşıma sarmalayıcısı."""
     return pencereyi_guvenli_tas(window, settings)
 
-class DraggableTransparentWindow(WindowLifecycleMixin, WindowMouseMixin, WindowSettingsMixin, WindowInitMixin, QtWidgets.QWidget, PencereGuncellemeKarishimi, PencereNavigasyonKarishimi, SerbestDuzenKarishimi):
+class DraggableTransparentWindow(WindowLifecycleMixin, WindowMouseMixin, WindowSettingsMixin, WindowInitMixin, WindowRuntimeMixin, QtWidgets.QWidget, PencereGuncellemeKarishimi, PencereNavigasyonKarishimi, SerbestDuzenKarishimi):
     def __init__(self, settings):
         super().__init__()
         self.settings = settings
@@ -52,36 +53,7 @@ class DraggableTransparentWindow(WindowLifecycleMixin, WindowMouseMixin, WindowS
 
 
 
-    def _zamanlayicilari_kur(self):
-        self.timer = QtCore.QTimer(self)
-        self.timer.timeout.connect(self.update_time)
-        self.timer.start(200)
 
-        self.batt_timer = QtCore.QTimer(self)
-        self.batt_timer.timeout.connect(self.update_battery)
-        self.batt_timer.start(5000)
-
-        self.full_charge_timer = QtCore.QTimer(self)
-        self.full_charge_timer.timeout.connect(self._toggle_full_charge_blink)
-        self.full_charge_timer.setInterval(500)
-
-        self.low_batt_timer = QtCore.QTimer(self)
-        self.low_batt_timer.timeout.connect(self._toggle_low_batt_blink)
-        self.low_batt_timer.setInterval(500)
-
-        self.hatirlatici_timer = QtCore.QTimer(self)
-        self.hatirlatici_timer.timeout.connect(self.hatirlaticilari_kontrol_et)
-        self.hatirlatici_timer.start(30000)
-
-        self.setContextMenuPolicy(QtCore.Qt.ContextMenuPolicy.CustomContextMenu)
-        self.customContextMenuRequested.connect(self.show_menu)
-
-    def _servisleri_baslat(self):
-        self.pil_servisi = PilServisi(dusuk_esik=self.settings.battery_warning_level)
-        self.bildirim_servisi = BildirimServisi()
-        self.hatirlatici_servisi = HatirlaticiServisi()
-        self.gorev_servisi = GorevServisi()
-        self._aktif_popuplar = {}
 
     # ---------- AYARLAR ---------- *****************
 
