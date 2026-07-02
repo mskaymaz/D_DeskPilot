b3df7f0bea69d77a399b038d836751e64ef78050
MSK <mustafasalihkaymaz@gmail.com>
2026-07-02 11:12:25 +0300

    Extract window initialization helpers

--- core_window.py
+++ core_window.py
@@ -30,13 +30,14 @@ from pencere_navigasyon import PencereNavigasyonKarishimi
 from window_mouse_mixin import WindowMouseMixin
 from window_lifecycle_mixin import WindowLifecycleMixin
 from window_settings_mixin import WindowSettingsMixin
+from window_init_mixin import WindowInitMixin
 from serbest_duzen import SerbestDuzenKarishimi
 
 def move_window_safely(window, settings):
     """DeskPilot.py tarafından kullanılan güvenli taşıma sarmalayıcısı."""
     return pencereyi_guvenli_tas(window, settings)
 
-class DraggableTransparentWindow(WindowLifecycleMixin, WindowMouseMixin, WindowSettingsMixin, QtWidgets.QWidget, PencereGuncellemeKarishimi, PencereNavigasyonKarishimi, SerbestDuzenKarishimi):
+class DraggableTransparentWindow(WindowLifecycleMixin, WindowMouseMixin, WindowSettingsMixin, WindowInitMixin, QtWidgets.QWidget, PencereGuncellemeKarishimi, PencereNavigasyonKarishimi, SerbestDuzenKarishimi):
     def __init__(self, settings):
         super().__init__()
         self.settings = settings
@@ -46,91 +47,10 @@ class DraggableTransparentWindow(WindowLifecycleMixin, WindowMouseMixin, WindowS
         self._init_widgets()
         self._init_startup()
 
-    def _init_state(self):
-        self.drag_pos = None
-        self._full_charge_blink_on = False
-        self._last_low_batt_alert_ts = 0
-        self._low_batt_blink_on = False
-        self._last_full_batt_alert_ts = 0
-        self.free_time_window = None
-        self.free_date_window = None
-        self.free_battery_window = None
-        self._free_layout_active = False
-        self._keep_top_timer = None
 
-    def _init_logging(self):
-        log_altyapisini_kur()
-        log_kaydet("Uygulama baslatildi.")
 
-    def _init_window(self):
-        flags = QtCore.Qt.WindowType.FramelessWindowHint | QtCore.Qt.WindowType.Window
-        if self.settings.her_zaman_ustte:
-            flags |= QtCore.Qt.WindowType.WindowStaysOnTopHint
-        self.setWindowFlags(flags)
-        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
-        self.setWindowOpacity(self.settings.seffaflik)
-        self.setWindowIcon(QtGui.QIcon(resource_path(ICON_FILE)))
 
-    def _init_widgets(self):
-        self.date_label = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.battery_label = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.battery_icon_label = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.battery_icon_label.setVisible(False)
 
-        self.battery_row = QtWidgets.QWidget()
-        self.battery_row_layout = QtWidgets.QHBoxLayout(self.battery_row)
-        self.battery_row_layout.setContentsMargins(0, 0, 0, 0)
-        self.battery_row_layout.setSpacing(4)
-        self.battery_row_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        self.battery_row_layout.addWidget(self.battery_label)
-        self.battery_row_layout.addWidget(self.battery_icon_label)
-
-        self.main_layout = QtWidgets.QVBoxLayout(self)
-        self.main_layout.setContentsMargins(0, 0, 0, 0)
-        self.main_layout.setSpacing(0)
-
-        self.spacer_bt = QtWidgets.QSpacerItem(
-            0,
-            self.settings.spacing_battery_time,
-            QtWidgets.QSizePolicy.Policy.Minimum,
-            QtWidgets.QSizePolicy.Policy.Fixed
-        )
-        self.spacer_td = QtWidgets.QSpacerItem(
-            0,
-            self.settings.spacing_time_date,
-            QtWidgets.QSizePolicy.Policy.Minimum,
-            QtWidgets.QSizePolicy.Policy.Fixed
-        )
-        self.time_label = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-
-        self.main_layout.addWidget(self.battery_row)
-        self.main_layout.addItem(self.spacer_bt)
-        self.main_layout.addWidget(self.time_label)
-        self.main_layout.addItem(self.spacer_td)
-        self.main_layout.addWidget(self.date_label)
-
-        for lbl in (
-            self.time_label,
-            self.date_label,
-            self.battery_label,
-            self.battery_icon_label
-        ):
-            lbl.setAttribute(QtCore.Qt.WidgetAttribute.WA_TransparentForMouseEvents)
-            lbl.setContentsMargins(0, 0, 0, 0)
-            lbl.setStyleSheet("""
-                QLabel {
-                    padding: 0px;
-                    margin: 0px;
-                }
-            """)
-
-    def _init_startup(self):
-        self._zamanlayicilari_kur()
-        self._servisleri_baslat()
-        self._setup_keep_on_top()
-        self.apply_settings()
-        self.update_time()
-        self.update_battery()
 
     def _zamanlayicilari_kur(self):
         self.timer = QtCore.QTimer(self)
---
+++ window_init_mixin.py
@@ -0,0 +1,95 @@
+try:
+    from PySide6 import QtCore, QtGui, QtWidgets
+except ImportError:
+    from PyQt6 import QtCore, QtGui, QtWidgets
+
+from core_utils import resource_path, ICON_FILE
+from core_logging import log_altyapisini_kur, log_kaydet
+
+
+class WindowInitMixin:
+    def _init_state(self):
+        self.drag_pos = None
+        self._full_charge_blink_on = False
+        self._last_low_batt_alert_ts = 0
+        self._low_batt_blink_on = False
+        self._last_full_batt_alert_ts = 0
+        self.free_time_window = None
+        self.free_date_window = None
+        self.free_battery_window = None
+        self._free_layout_active = False
+        self._keep_top_timer = None
+
+    def _init_logging(self):
+        log_altyapisini_kur()
+        log_kaydet("Uygulama baslatildi.")
+
+    def _init_window(self):
+        flags = QtCore.Qt.WindowType.FramelessWindowHint | QtCore.Qt.WindowType.Window
+        if self.settings.her_zaman_ustte:
+            flags |= QtCore.Qt.WindowType.WindowStaysOnTopHint
+        self.setWindowFlags(flags)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground)
+        self.setWindowOpacity(self.settings.seffaflik)
+        self.setWindowIcon(QtGui.QIcon(resource_path(ICON_FILE)))
+
+    def _init_widgets(self):
+        self.date_label = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.battery_label = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.battery_icon_label = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.battery_icon_label.setVisible(False)
+
+        self.battery_row = QtWidgets.QWidget()
+        self.battery_row_layout = QtWidgets.QHBoxLayout(self.battery_row)
+        self.battery_row_layout.setContentsMargins(0, 0, 0, 0)
+        self.battery_row_layout.setSpacing(4)
+        self.battery_row_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.battery_row_layout.addWidget(self.battery_label)
+        self.battery_row_layout.addWidget(self.battery_icon_label)
+
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
+        self.main_layout.setSpacing(0)
+
+        self.spacer_bt = QtWidgets.QSpacerItem(
+            0,
+            self.settings.spacing_battery_time,
+            QtWidgets.QSizePolicy.Policy.Minimum,
+            QtWidgets.QSizePolicy.Policy.Fixed
+        )
+        self.spacer_td = QtWidgets.QSpacerItem(
+            0,
+            self.settings.spacing_time_date,
+            QtWidgets.QSizePolicy.Policy.Minimum,
+            QtWidgets.QSizePolicy.Policy.Fixed
+        )
+        self.time_label = QtWidgets.QLabel(alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        self.main_layout.addWidget(self.battery_row)
+        self.main_layout.addItem(self.spacer_bt)
+        self.main_layout.addWidget(self.time_label)
+        self.main_layout.addItem(self.spacer_td)
+        self.main_layout.addWidget(self.date_label)
+
+        for lbl in (
+            self.time_label,
+            self.date_label,
+            self.battery_label,
+            self.battery_icon_label
+        ):
+            lbl.setAttribute(QtCore.Qt.WidgetAttribute.WA_TransparentForMouseEvents)
+            lbl.setContentsMargins(0, 0, 0, 0)
+            lbl.setStyleSheet("""
+                QLabel {
+                    padding: 0px;
+                    margin: 0px;
+                }
+            """)
+
+    def _init_startup(self):
+        self._zamanlayicilari_kur()
+        self._servisleri_baslat()
+        self._setup_keep_on_top()
+        self.apply_settings()
+        self.update_time()
+        self.update_battery()
